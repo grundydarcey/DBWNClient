30fe42ee9b6af2b7a86cc1ca4ac7a4c2f9973490
Beyonce Knowles <you@example.com>
2021-04-13 16:16:25 -0400

    nav

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
+const [showMenu, setShowMenu] = useState(false);
+
+let menu
+let menuMask
+if(showMenu) {
+  menu  = 
+  <div className='menutext'>
+    <p><Link to='/'>Home</Link></p>
+    <p><Link to='/restaurants'>Restaurants</Link></p>
+    <p><Link to='/nightlife'>Nightlife</Link></p>
+    <p><Link to='/map'>Map</Link></p> 
+    <p><Link to='/donate'>Donate</Link></p>
+  </div>
+}
+
+menuMask = 
+<div className='mask'>
+</div>
+
   return (
     <nav className='headernav'>
       <section className='nav'>
+        <section className='hamburger'>
+        <FontAwesomeIcon
+          icon={faBars}
+          onClick={() => setShowMenu(!showMenu)}
+        />
+        </section>
+        {menuMask}
+        {menu}
         <p><Link to='/'>Home</Link></p>
         <p><Link to='/restaurants'>Restaurants</Link></p>
         <p><Link to='/nightlife'>Nightlife</Link></p>
